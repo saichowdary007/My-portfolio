f0e1ebba0dbc22942074df423fe9f90bf51dc13e
saichowdary007 <pakalapatisaikumarchowdary5.com>
2025-04-08 13:00:02 -0500

    added logos

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -23,7 +23,7 @@ export default function Skills() {
               Scikit-learn
             </li>
             <li className="flex items-center">
-              <img src="https://cdn.simpleicons.org/matplotlib" alt="Matplotlib" className="h-5 w-5 mr-2" />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Matplotlib_icon.svg" alt="Matplotlib" className="h-5 w-5 mr-2" />
               Matplotlib
             </li>
           </ul>
@@ -47,11 +47,11 @@ export default function Skills() {
           <h3 className="text-xl font-semibold mb-4 border-b-2 border-zinc-800 pb-2">Visualization</h3>
           <ul className="space-y-2">
             <li className="flex items-center">
-              <img src="https://cdn.simpleicons.org/powerbi/F2C811" alt="Power BI" className="h-5 w-5 mr-2" />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/c/cf/New_Power_BI_Logo.svg" alt="Power BI" className="h-5 w-5 mr-2" />
               Power BI
             </li>
             <li className="flex items-center">
-              <img src="https://cdn.simpleicons.org/tableau/E97627" alt="Tableau" className="h-5 w-5 mr-2" />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/4/4b/Tableau_Logo.png" alt="Tableau" className="h-5 w-5 mr-2" />
               Tableau
             </li>
             <li className="flex items-center">
@@ -73,7 +73,7 @@ export default function Skills() {
               MongoDB
             </li>
             <li className="flex items-center">
-              <img src="https://cdn.simpleicons.org/oracle/F80000" alt="Oracle" className="h-5 w-5 mr-2" />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg" alt="Oracle" className="h-5 w-5 mr-2" />
               Oracle
             </li>
             <li className="flex items-center">
@@ -91,7 +91,7 @@ export default function Skills() {
           <h3 className="text-xl font-semibold mb-4 border-b-2 border-zinc-800 pb-2">Development</h3>
           <ul className="space-y-2">
             <li className="flex items-center">
-              <img src="https://skillicons.dev/icons?i=jupyter" alt="Jupyter" className="h-5 w-5 mr-2" />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/3/38/Jupyter_logo.svg" alt="Jupyter" className="h-5 w-5 mr-2" />
               Jupyter
             </li>
             <li className="flex items-center">
@@ -103,7 +103,7 @@ export default function Skills() {
               PyCharm
             </li>
             <li className="flex items-center">
-              <img src="https://cdn.simpleicons.org/rstudio/75AADB" alt="R Studio" className="h-5 w-5 mr-2" />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/RStudio_logo_flat.svg" alt="R Studio" className="h-5 w-5 mr-2" />
               R Studio
             </li>
           </ul>
